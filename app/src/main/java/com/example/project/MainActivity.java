a6f8a85f7cdc4e5dff829c10b7b9acac54f7968e
nsa <b21willu@student.his.se>
2022-05-03 14:52:14 +0200

    Wrote code that read data from Shared Preferences in MainActivity. Created SecondActivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewName;
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textViewName = findViewById(R.id.name);
+
+        preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("name", "Wilmer");
+        editor.apply();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String name = preferences.getString("name", "inget namn hittades");
+        textViewName.setText(name);
+    }
 }
